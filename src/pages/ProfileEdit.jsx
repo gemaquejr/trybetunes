82fc6df7d8a045e0d993d7d0961ab95998fefc90
gemaquejr <gemaquejr@hotmail.com>
2021-11-22 18:42:45 -0300

    Adiciona Requisito 1

---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        ProfileEdit
+      </div>
+
+    );
+  }
+}
