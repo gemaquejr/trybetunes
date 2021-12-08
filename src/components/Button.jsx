0f21cfc8f0962ed88043536c05b148f3743505fe
gemaquejr <gemaquejr@hotmail.com>
2021-12-08 11:37:01 -0300

    Adiciona Requisito 2

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = () => (
+  <button
+    type="submit"
+    onClick={ () => setInputName }
+    className="button"
+  >
+    Adicionar
+  </button>
+);
+
+export default Button;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
-  render() {
-    return (
-      <div data-testid="page-login">
-        Login
-      </div>
-
-    );
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: false,
+      isDisabled: true,
+      returnResponse: false,
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    const enableButton = 3;
+    this.setState(() => ({ [name]: value, isDisabled: value.length < enableButton }));
+  }
+
+  handleSearch = async () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    const response = await createUser({ name });
+    if (response === 'OK') {
+      this.setState({ loading: false, returnResponse: true });
+    }
+  }
+
+  render = () => {
+    const { name, loading, isDisabled, returnResponse } = this.state;
+    return returnResponse ? (
+      <Redirect to="/search" />)
+      : (
+        <div data-testid="page-login">
+          {loading ? <Loading /> : (
+            <form>
+              <label htmlFor="userName">
+                <input
+                  data-testid="login-name-input"
+                  name="name"
+                  value={ name }
+                  type="text"
+                  placeholder="Nome"
+                  onChange={ (event) => this.handleInput(event) }
+                />
+              </label>
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ isDisabled }
+                onClick={ (event) => this.handleSearch(event) }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+        </div>
+      );
+  };
 }
