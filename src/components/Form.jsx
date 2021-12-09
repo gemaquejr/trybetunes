686f38335e82d712da01adaf8ef4ce8d2d94f4ac
gemaquejr <gemaquejr@hotmail.com>
2021-12-09 10:25:09 -0300

    Adiciona Requisito 5

---
+++ src/components/Form.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class Loading extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    const enableButton = 2;
+    this.setState(() => ({
+      [name]: value,
+      isDisabled: value.length < enableButton,
+    }));
+  };
+
+  render() {
+    const { isDisabled } = this.state;
+    return (
+      <div>
+        <label htmlFor="searchArtist">
+          <form>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              placeholder="Artista"
+              onChange={ (event) => this.handleInput(event) }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              disabled={ isDisabled }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </label>
+      </div>
+    );
+  }
+}
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Form from '../components/Form';
 
 export default class Search extends Component {
   render() {
     return (
       <div data-testid="page-search">
         <Header />
+        <Form />
         Search
       </div>
 
