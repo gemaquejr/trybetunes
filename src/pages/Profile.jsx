95c62e56e8c66ba8ca7503c663491d6ffe70258b
gemaquejr <gemaquejr@hotmail.com>
2021-12-22 04:15:02 -0300

    Adiciona Requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.userProfile();
+  }
+
+  userProfile() {
+    this.setState({ loading: true });
+    getUser().then((user) => {
+      this.setState({ user, loading: false });
+    });
+  }
+
   render() {
+    const { user, loading } = this.state;
     return (
       <div data-testid="page-profile">
         <h1>TrybeTunes</h1>
         <Header />
+        {loading ? <Loading /> : (
+          <>
+            <Link to="/profile/edit">Editar perfil</Link>
+            <img data-testid="profile-image" src={ user.image } alt={ user.name } />
+            <h1>{ user.name }</h1>
+            <h1>{ user.email }</h1>
+            <h1>{ user.description }</h1>
+          </>
+        )}
       </div>
-
     );
   }
 }
