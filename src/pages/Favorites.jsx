4be99ba5644343d2bcca7a5be11ca614d4daf2ee
gemaquejr <gemaquejr@hotmail.com>
2023-03-12 17:43:27 -0300

    Adiciona refatoracao da pagina Favorites.jsx

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,14 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
-export default class Favorites extends Component {
+class Favorites extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoanding: true,
+      songs: [],
+    };
+
+    this.loadFavoritesSongs = this.loadFavoritesSongs.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadFavoritesSongs();
+  }
+
+  async loadFavoritesSongs() {
+    const songs = await getFavoriteSongs();
+    this.setState({ isLoanding: false, songs });
+  }
+
   render() {
-    return (
-      <div data-testid="page-favorites">
-        <h1>TrybeTunes</h1>
-        <Header />
-      </div>
+    const { isLoanding, songs } = this.state;
 
+    return (
+      <div className="page-favorites" data-testid="page-favorites">
+        <Header />
+
+        {
+          isLoanding ? <Loading /> : (
+            <>
+              <h1 className="favorites-title">Músicas Favoritas:</h1>
+
+              {
+                songs.map((song) => (
+                  <MusicCard
+                    audioComponent="audio-component"
+                    checkboxMusicTrackId={ `checkbox-music-${song.trackId}` }
+                    key={ song.trackId }
+                    loadFavoritesSongs={ this.loadFavoritesSongs }
+                    previewUrl={ song.previewUrl }
+                    song={ song }
+                    trackId={ song.trackId }
+                    trackName={ song.trackName }
+                  />
+                ))
+              }
+            </>
+          )
+        }
+      </div>
     );
   }
 }
+
+export default Favorites;
