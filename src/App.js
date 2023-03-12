10135a52264e6ed7216dfbf0f2032bca40508550
gemaquejr <gemaquejr@hotmail.com>
2023-03-12 18:38:47 -0300

    Adiciona inicio do CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -8,6 +7,7 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import './style.css';
 
 class App extends React.Component {
   render() {
